14ee87a36d539829f46a88d29673e601732048bd
Marc8s <marcus.ecomp@gmail.com>
2021-03-20 20:12:43 +0000

    Standart slide (simpleslide)

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Slider inicial
 # # DONE
 1) Configuração do Firebase (realtime database e authentication)
 2) Configuração do material intro (https://github.com/heinrichreimer/material-intro)
+3) Standart slide (SimpleSlide)
--- app/src/main/java/com/example/organnize/MainActivity.java
+++ app/src/main/java/com/example/organnize/MainActivity.java
@@ -5,12 +5,40 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        /*Standart slide (SimpleSlide)
+        //remove butões back e next dos slides
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo")
+                .description("Descrição")
+                .image(R.drawable.um)
+                .background(R.color.purple_200)
+                .build()
+        );
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo2")
+                .description("Descrição2")
+                .image(R.drawable.dois)
+                .background(R.color.purple_200)
+                .build()
+        );
+        addSlide(new SimpleSlide.Builder()
+                .title("Titulo3")
+                .description("Descrição3")
+                .image(R.drawable.tres)
+                .background(R.color.purple_200)
+                .build()
+        );*/
     }
 }
