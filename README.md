4419e27e79e40e32581eb5f575623783cffdbc4b
Marc8s <marcus.ecomp@gmail.com>
2021-03-20 21:40:25 +0000

    Inclusão dos 4 primeiros sliders

--- README.md
+++ README.md
@@ -7,4 +7,4 @@ Slider inicial
 # # DONE
 1) Configuração do Firebase (realtime database e authentication)
 2) Configuração do material intro (https://github.com/heinrichreimer/material-intro)
-3) Standart slide (SimpleSlide)
+3) Inclusão dos 4 primeiros Fragment Slide
--- app/src/main/java/com/example/organnize/MainActivity.java
+++ app/src/main/java/com/example/organnize/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
 import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
@@ -14,31 +15,29 @@ public class MainActivity extends IntroActivity {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
 
-        /*Standart slide (SimpleSlide)
         //remove butões back e next dos slides
         setButtonBackVisible(false);
         setButtonNextVisible(false);
 
-        addSlide(new SimpleSlide.Builder()
-                .title("Titulo")
-                .description("Descrição")
-                .image(R.drawable.um)
+        addSlide(new FragmentSlide.Builder()
                 .background(R.color.purple_200)
+                .fragment(R.layout.intro_1)
                 .build()
         );
-        addSlide(new SimpleSlide.Builder()
-                .title("Titulo2")
-                .description("Descrição2")
-                .image(R.drawable.dois)
+        addSlide(new FragmentSlide.Builder()
                 .background(R.color.purple_200)
+                .fragment(R.layout.intro_2)
                 .build()
         );
-        addSlide(new SimpleSlide.Builder()
-                .title("Titulo3")
-                .description("Descrição3")
-                .image(R.drawable.tres)
+        addSlide(new FragmentSlide.Builder()
                 .background(R.color.purple_200)
+                .fragment(R.layout.intro_3)
                 .build()
-        );*/
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(R.color.purple_200)
+                .fragment(R.layout.intro_4)
+                .build()
+        );
     }
 }
